3cb3718700acaf0a65b3351814d643660a838a5b
Nemo21 <saggubableen9@gmail.com>
2023-05-20 12:46:22 +0530

    GoogleDocs

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import TextEditor from "./TextEditor";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
+import { Redirect } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to={`/documents/${uuidV4()}`} />
+        </Route>
+        <Route path="/documents/:id">
+          <TextEditor />;
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
